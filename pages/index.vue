9d0ca5fbdc68bc6aaadab2619e0a68ee2ccd0f06
malred <13695024677@163.com>
2023-01-08 17:02:40 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+ async asyncData({$axios}) {
+    const url = 'http://localhost:9090/test'
+    const res = await $axios.$get(url)
+    console.log(res);
+  },
+}
+</script>
