baa4c55c177d1f577bf5a5c66a3e72585a1590a9
malred <13695024677@163.com>
2023-01-12 06:46:44 +0800

    根路由

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,17 @@
 export default {
+  router: {
+    base: '/',
+    // 添加路由
+    // routes路由规则数组
+    // resolve解析路由组件路径
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: '/hello',
+        path: 'hello',
+        component: resolve(__dirname, 'pages/about.vue')
+      })
+    }
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'proj',
@@ -6,13 +19,13 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
@@ -30,8 +43,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
