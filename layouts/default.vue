fc74038488233d4b0b7269a9b92594a3273f99c7
malred <13695024677@163.com>
2023-01-12 08:21:46 +0800

    10-asyncData

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>layouts/default.vue</h1>
+    <div class="bottom-nav">
+<!--      <span>1</span>-->
+<!--      <span>1</span>-->
+<!--      <span>1</span>-->
+    </div>
     <!--  页面出口,类似子路由  -->
     <nuxt/>
   </div>
@@ -13,5 +17,15 @@ export default {
 </script>
 
 <style scoped>
+.bottom-nav {
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  background: lightskyblue;
 
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>111</div>
+  <div>{{ test }}</div>
 </template>
 
 <script>
 export default {
- async asyncData({$axios}) {
-    const url = 'http://localhost:9090/test'
-    const res = await $axios.$get(url)
-    console.log(res);
+  // 获取服务端渲染的数据
+  // 在服务端和客户端渲染前执行
+  async asyncData({$axios}) {
+    // 获取本地static文件夹下的文件
+    const url = 'http://localhost:3000/data.json'
+    const res = await $axios({
+      url,
+      method: 'get'
+    })
+    // 这里导出的数据(和data(){}返回的数据混合并导出)可以直接使用
+    return res.data
   },
 }
 </script>
