e84ad86492fe4b7652e9e74ab95ef9d2d4502530
malred <13695024677@163.com>
2023-01-12 08:03:17 +0800

    视图--布局组件

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>layouts/default.vue</h1>
+    <!--  页面出口,类似子路由  -->
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault'
+}
+</script>
+
+<style scoped>
+
+</style>
